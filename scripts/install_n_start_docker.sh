a5123949a6a1896deaa89106374aaa847cb44ce0
Nahak, Sandip <Sandip_Nahak@intuit.com>
2020-11-13 17:08:23 +0530

    updating appspec

--- scripts/install_n_start_docker.sh
+++ scripts/install_n_start_docker.sh
@@ -6,6 +6,5 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo mkdir -p /tinyurl 
 sudo chown ec2-user:ec2-user /tinyurl
-sudo unzip /deploy/gotinyurl.zip -d /tinyurl
 sudo chown -R ec2-user:ec2-user /tinyurl
 sudo chmod 777 gourlshortner*
