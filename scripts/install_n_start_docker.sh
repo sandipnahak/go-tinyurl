a0b4320c62b1126054f05752befd131f675a2a60
Nahak, Sandip <Sandip_Nahak@intuit.com>
2020-11-13 17:24:24 +0530

    updating appspec

--- scripts/install_n_start_docker.sh
+++ scripts/install_n_start_docker.sh
@@ -5,6 +5,5 @@ sudo yum install docker -y
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo mkdir -p /tinyurl 
-sudo chown ec2-user:ec2-user /tinyurl
-sudo chown -R ec2-user:ec2-user /tinyurl
+sudo unzip  $DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/bundle.tar  -d /tinyurl
 sudo chmod 777 gourlshortner*
